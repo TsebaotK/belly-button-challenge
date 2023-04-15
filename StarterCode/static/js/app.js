4aa6d36b2406422ffc22645a1a17712fe88f147f
Tsebaot Kebede <118030918+TsebaotK@users.noreply.github.com>
2023-04-14 21:45:48 -0400

    main file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,71 +1,114 @@
 var url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 
-// Fetch the JSON data 
-var datas = d3.json(url).then(function(data) {
-  console.log("datas: ", data);
-});
 
-// For loop to populate arrays
-for (var i = 0; i < datas.length; i++) { 
-    // Create items array
-    var items = Object.keys(datas).map(function(key) {
-      return [key, datas[samples[sample_values]]];
-    });
-    console.log("items: ", items);
+// Initialization function
+function initialization() {
+  // Fetch the names JSON data 
+  d3.json(url).then((data) => {
+    names = data.names;
 
-    // Sort the array based on the second element
-    var sorted = items.sort(function (first, second) {
-      return second[1] - first[1];
-    });
-    console.log("sorted: ", sorted);
-
-    // Reverse the array to accommodate Plotly's defaults
-    var reversedArray = sorted.reverse();
-    console.log("reversed Array: ", reversedArray);
-
-    // Create a new array with only the first 10 items
-    var slicedData = console.log(reversedArray.slice(0, 10));
-    console.log("sliced Data: ", slicedData);
-
-  
-    // ----
-    var trace1 = {
-        x: slicedData.map(row => row.samples[sample_values]),
-        y: slicedData.map(row => row.otu_idsThan),
-        text: slicedData.map(object => object.otu_labels),
-        type: 'bar',
-        orientation: "h"
-    }
-
-    // ----
-    var trace2 = {
-      x: datas.otu_ids,
-      y: datas.sample_values,
-      text: slicedData.otu_labels,
-      mode: 'markers',
-      marker: {
-          size: slicedData.sample_values,
-          color: slicedData.otu_ids
-      }
-    }
-}
-// Data trace array
-var barChart = [trace1];
-var bubbleChart = [trace2];
-// var guageChart = [trace3];
-
-let layout = {
-  title: "title"
+    let name = d3.select("#selDataset");
+    for (let i = 0; i < names.length; i++) {
+      name.append("option").attr("value", "names").text(names[i]);
+      // console.log(names)
+    }    
+  })
 };
 
-
-// Render the plot to the div tag with id "bar"
-Plotly.newPlot("bar", barChart, layout);
-
-// Render the plot to the div tag with id "gauge"
-Plotly.newPlot("gauge", guageChart, layout);
+initialization()
 
 
-// Render the plot to the div tag with id "bubble"
-Plotly.newPlot("bubble", bubbleChart, layout);
+// Fetch the metadata JSON object 
+d3.json(url).then((data) => {
+  metadata = data.metadata
+
+  let demoInfo = d3.select(".panel-body");
+  // For loop to populate metadata arrays
+  for (let i = 0; i < metadata.length; i++) {
+    row = metadata[i];
+    if (row.id == initialization[i]) {
+      console.log("id", [i], ":", row.id[i])
+      console.log("Ethnicity", [i], ":", row.ethnicity[i])
+      console.log("gender", [i], ":", row.gender[i])
+      console.log("age", [i], ":", row.age[i])
+      console.log("location", [i], ":", row.location[i])
+      console.log("bbtyoe", [i], ":", row.bbtype[i])
+      console.log("wfreq", [i], ":", row.wfreq[i])
+      demoInfo.append("ul").text(row.id);
+      demoInfo.append("ul").text(row.ethnicity);
+      demoInfo.append("ul").text(row.gender);
+      demoInfo.append("ul").text(row.age);
+      demoInfo.append("ul").text(row.location);
+      demoInfo.append("ul").text(row.bbtype);
+      demoInfo.append("ul").text(row.wfreq);
+    }
+  }
+}
+  // }
+);
+
+
+
+
+// Fetch the samples JSON object 
+d3.json(url).then((data) => {
+
+  // Initialized arrays
+  samples = data.samples
+
+  let sample = d3.select("#bar");
+  // For loop to populate arrays
+  for (let i = 0; i < samples.length; i++) {
+    row = samples[i];
+    if (row.id == initialization()) {
+      console.log(row.id);
+      console.log(row.otu_ids);
+      console.log(row.sample_values);
+      console.log(row.otu_labels);
+      // sample.append("ul").text(row.id);
+      // sample.append("ul").text(row.id);
+      let trace1 = {
+        x: row.sample_values[0],
+        y: row.otu_ids[0],
+        text: row.otu_labels[0],
+        type: 'bar',
+        orientation: "h"
+      };
+
+      let trace2 = {
+        x: row.otu_ids[0],
+        y: row.sample_values[0],
+        text: row.otu_labels[0],
+        mode: 'markers',
+        marker: {
+          size: row.sample_values[0],
+          color: row.otu_ids[0]
+        }
+      };
+
+      // Data trace array
+      let barChart = [trace1];
+      let bubbleChart = [trace2];
+      // var guageChart = [trace3];
+
+      // Render the plot to the div tag with id "bar"
+      Plotly.newPlot("bar", barChart, layout);
+
+      // // Render the plot to the div tag with id "gauge"
+      // // Plotly.newPlot("gauge", guageChart);
+
+
+      // Render the plot to the div tag with id "bubble"
+      Plotly.newPlot("bubble", bubbleChart, layout);
+      // }
+    }
+  }
+});
+
+
+
+
+
+
+
